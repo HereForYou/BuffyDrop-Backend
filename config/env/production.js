a597df7555996e706e00706d047aef2da5052ee6
JamesLee <jameslee82027@gmail.com>
2024-09-14 20:25:58 +0900

    fix:bot token, start

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: 5000,
     // db: "mongodb://localhost:27017/Bleggs",
-    db: "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.10",
+    // db: "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.10",
+    db: " mongodb + srv://jameslee82027:jBbc4uISlTVpqmRK@dogbot.bwuh3.mongodb.net/"
 }
