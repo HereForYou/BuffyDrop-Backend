b55aa67f99c707540635fd353e49301c79d5fa5d
HopeStar4453 <jameslee82027@gmail.com>
2024-09-17 01:38:25 +0900

    fix: mongodb

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: 5000,
-    db: "mongodb://localhost:27017/Bleggs",
-    // db: "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.10",
+    // db: "mongodb://localhost:27017/Bleggs",
+    db: "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.10",
 }
